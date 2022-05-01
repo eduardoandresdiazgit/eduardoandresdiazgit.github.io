d466726dc2bf02cf80fe508d7facaa86941cedf5
eduardoandresdiazgit <104528720+eduardoandresdiazgit@users.noreply.github.com>
2022-04-30 22:41:13 -0300

    Add files via upload

--- internet.js
+++ internet.js
@@ -1,11 +1,1 @@
-const isOnline = require('is-online');
-setInterval(myTimer, 1000);
-
-function myTimer() {
-  (async() => {
-  	console.log(await isOnline());
-    console.log(Date.now());
-    aaa=await isOnline();
-  //  document.getElementById("putodemierda").innerHTML = date.toLocaleTimeString();
-  })();
-}
+html
